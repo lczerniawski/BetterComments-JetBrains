b88e7ca55d16b4a958168e2cdda207b1012f94f0
Lukasz Czerniawski <l.czerniawski@icloud.com>
2024-10-29 18:36:43 +0100

    Fix update of coloring to be aligned with current state of document

--- src/main/kotlin/com/lczerniawski/bettercomments/HighlightDocumentListener.kt
+++ src/main/kotlin/com/lczerniawski/bettercomments/HighlightDocumentListener.kt
@@ -1,12 +1,15 @@
 package com.lczerniawski.bettercomments
 
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
-import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiDocumentManager
 
 
 class HighlightDocumentListener(private val editor: Editor) : DocumentListener {
     override fun documentChanged(event: DocumentEvent) {
+        val project = editor.project ?: return
+        PsiDocumentManager.getInstance(project).commitDocument(event.document)
         CommentsHighlighter.applyCustomHighlighting(editor)
     }
 
