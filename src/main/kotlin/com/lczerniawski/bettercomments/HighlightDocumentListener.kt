75ffe718a1abdaa079f9c3f730aadde8efab14cb
Lukasz Czerniawski <l.czerniawski@icloud.com>
2024-11-28 16:58:23 +0100

    Try to ix "Must not modify PSI inside save listener" exception once again.

--- src/main/kotlin/com/lczerniawski/bettercomments/HighlightDocumentListener.kt
+++ src/main/kotlin/com/lczerniawski/bettercomments/HighlightDocumentListener.kt
@@ -10,9 +10,9 @@ import com.intellij.psi.PsiDocumentManager
 class HighlightDocumentListener(private val editor: Editor) : DocumentListener {
     override fun documentChanged(event: DocumentEvent) {
         val project = editor.project ?: return
-        PsiDocumentManager.getInstance(project).commitDocument(event.document)
 
         WriteCommandAction.runWriteCommandAction(project) {
+            PsiDocumentManager.getInstance(project).commitDocument(event.document)
             CommentsHighlighter.applyCustomHighlighting(editor)
         }
     }
