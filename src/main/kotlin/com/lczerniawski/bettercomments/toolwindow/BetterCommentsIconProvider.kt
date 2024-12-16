4e69f4d5d68db24ba56ea97d5771925fd0ed8a2f
Lukasz Czerniawski <l.czerniawski@icloud.com>
2024-12-16 20:57:17 +0100

    Change API usage to be removed

--- src/main/kotlin/com/lczerniawski/bettercomments/toolwindow/BetterCommentsIconProvider.kt
+++ src/main/kotlin/com/lczerniawski/bettercomments/toolwindow/BetterCommentsIconProvider.kt
@@ -1,12 +1,13 @@
 package com.lczerniawski.bettercomments.toolwindow
 
 import com.intellij.openapi.util.IconLoader
-import com.intellij.util.ui.UIUtil
+import com.intellij.ui.JBColor
 import javax.swing.Icon
 
 class BetterCommentsIconProvider {
     fun getIcon(): Icon {
-        return if (UIUtil.isUnderDarcula()) {
+        val isDarkTheme = !JBColor.isBright()
+        return if (isDarkTheme) {
             IconLoader.getIcon("/icons/toolbar_icon_dark_13x13.svg", javaClass)
         } else {
             IconLoader.getIcon("/icons/toolbar_icon_light_13x13.svg", javaClass)
