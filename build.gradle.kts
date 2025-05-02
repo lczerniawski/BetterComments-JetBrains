60e8c28a2bfedeba047e1e23b117c4be64729cf4
Lukasz Czerniawski <l.czerniawski@icloud.com>
2025-05-02 13:06:47 +0200

    Change to the unlimited 'Until Build' setting.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.lczerniawski"
-version = "3.0.0"
+version = "3.0.1"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,6 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("231")
-        untilBuild.set("251.*")
     }
 
     signPlugin {
